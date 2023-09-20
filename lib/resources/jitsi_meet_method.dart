968cfeac84e15c71ce63162c99bb610c705ad7b1
DannyMarvel <dannymarvey@gmail.com>
2023-09-20 23:48:06 +0100

    further commit

--- lib/resources/jitsi_meet_method.dart
+++ lib/resources/jitsi_meet_method.dart
@@ -1,6 +1,6 @@
 import 'package:danny_zoom/resources/auth_methods.dart';
 import 'package:jitsi_meet_wrapper/jitsi_meet_wrapper.dart';
-import 'package:ji';
+
 
 class JitsiMeetMethods {
   final AuthMethods _authMethods = AuthMethods();
@@ -10,10 +10,10 @@ class JitsiMeetMethods {
     required bool isVideoMuted,
   }) async {
     try {
-      FeatureFlag featureFlag = FeatureFlag();
-      featureFlag.welcomePageEnabled = false;
-      featureFlag.resolution = FeatureFlagVideoResolution
-          .MD_RESOLUTION; // Limit video resolution to 360p
+      // FeatureFlag featureFlag = FeatureFlag();
+      // featureFlag.welcomePageEnabled = false; 
+      // featureFlag.resolution = FeatureFlagVideoResolution
+      //     .MD_RESOLUTION; // Limit video resolution to 360p
 
       var options = JitsiMeetingOptions(
         roomNameOrUrl: roomName,
